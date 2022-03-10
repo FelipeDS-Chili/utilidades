ceb321acff040705955ab76fb2cc32e629817834
FelipeInostroza <71255292+FelipeDS-Chili@users.noreply.github.com>
2022-03-10 20:30:57 -0300

    Update transformers.py

--- utilidades/transformers.py
+++ utilidades/transformers.py
@@ -1,6 +1,5 @@
 from sklearn.base import TransformerMixin
 from sklearn.base import BaseEstimator
-from sklearn.pipeline import Pipeline
 import pandas as pd
 import numpy as np
 from funciones import df_optimized
@@ -9,20 +8,18 @@ from funciones import df_optimized
 
 # Scalers para FECHA--> transformación a seno y coseno (CosSinMesHoraMinuto, CosSinDiaSemana)
 
+
 class CosSinMesHoraMinuto(TransformerMixin,BaseEstimator):
 
-    def __init__(self):
+    def __init__(self, input_col):
         
-        '''
-        Only requirement: have the column in the format: YY-MM-DD HH-MM-SS with datetime format
-        Returns: sin and cos columns
-        '''
+        self.input_col = input_col
         pass
 
-    def fit(self, X, columna_fecha, y=None):
+    def fit(self, X, y=None):
         return self
 
-    def transform(self, X, columna_fecha, y=None):
+    def transform(self, X, y=None):
 
         months_in_year = 12
         hours_in_day = 23
@@ -30,63 +27,89 @@ class CosSinMesHoraMinuto(TransformerMixin,BaseEstimator):
 
         X_return = pd.DataFrame()
 
-        X_return['sin_mes'] = np.sin(2*np.pi*X[columna_fecha].dt.month/months_in_year)
-        X_return['cos_mes'] = np.cos(2*np.pi*X[columna_fecha].dt.month/months_in_year)
+        X_return['sin_mes'] = np.sin(2*np.pi*X[self.input_col].dt.month/months_in_year)
+        X_return['cos_mes'] = np.cos(2*np.pi*X[self.input_col].dt.month/months_in_year)
 
-        X_return['sin_hora'] = np.sin(2*np.pi*X[columna_fecha].dt.hour/hours_in_day)
-        X_return['cos_hora'] = np.cos(2*np.pi*X[columna_fecha].dt.hour/hours_in_day)
 
-        X_return['sin_minute'] = np.sin(2*np.pi*X[columna_fecha].dt.minute/minutes_in_hour)
-        X_return['cos_minute'] = np.cos(2*np.pi*X[columna_fecha].dt.minute/minutes_in_hour)
+        X_return['sin_hora'] = np.sin(2*np.pi*X[self.input_col].dt.hour/hours_in_day)
+        X_return['cos_hora'] = np.cos(2*np.pi*X[self.input_col].dt.hour/hours_in_day)
+
+        X_return['sin_minute'] = np.sin(2*np.pi*X[self.input_col].dt.minute/minutes_in_hour)
+        X_return['cos_minute'] = np.cos(2*np.pi*X[self.input_col].dt.minute/minutes_in_hour)
+
 
         return np.array(X_return)
 
 
 class CosSinDiaSemana(TransformerMixin,BaseEstimator):
+
     def __init__(self):
-
-        '''
-        Transformer to pass the week day to a cyclical variable. 
-        Format = False --> The column contains the name of the days inside {Lunes, Martes, Miercoles..}
-        Format = True --> The column already contains the number of the day inside {1,2,3,4..}
-        columna_dia_semana --> column containing the days of week
-        '''
-
         pass
 
-    def fit(self, X, columna_dia_semana ,format = False, y=None):
+    def fit(self, X, y=None):
         return self
 
-    def transform(self, X, columna_dia_semana,format = False, y=None):
+    def transform(self, X, y=None):
 
         X_return = pd.DataFrame()
 
         days_in_week = 7
-        
-        if format:
-            X_return['sin_dia_semana'] = np.sin(2*np.pi*X.columna_dia_semana/days_in_week)
-            X_return['cos_dia_semana'] = np.cos(2*np.pi*X.columna_dia_semana/days_in_week)
-            return np.array(X_return)
-        
-        else:
-            dias_num = {'Lunes':1, 'Martes': 2, 'Miercoles': 3 , 'Jueves': 4, 'Viernes': 5, 'Sabado': 6, 'Domingo': 7}
-            X_return[columna_dia_semana] = X.DIANOM.map(dias_num)
-
-            X_return['sin_dia_semana'] = np.sin(2*np.pi*X_return.columna_dia_semana/days_in_week)
-            X_return['cos_dia_semana'] = np.cos(2*np.pi*X_return.columna_dia_semana/days_in_week)
-            return np.array(X_return.drop(columns = [columna_dia_semana]))
-        
 
 
-class OptimizeDf(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        '''
-        Downcast the data continously in the pipeline.
-        Recommended to use after a ColumnTransformer
-        '''
+        X_return['sin_dia_semana'] = np.sin(2*np.pi*X.Dia_de_Semana/days_in_week)
+        X_return['cos_dia_semana'] = np.cos(2*np.pi*X.Dia_de_Semana/days_in_week)
+
+
+        return np.array(X_return)
+
+
+class DegreeTransformer(TransformerMixin,BaseEstimator):
+
+    def __init__(self, input_col):
+
+        self.input_col = input_col
         pass
-    def transform(self, X, y=None):
-        X_ = pd.DataFrame(X)
-        return np.array(df_optimized(X_))
+
     def fit(self, X, y=None):
         return self
+
+    def transform(self, X, y=None):
+
+        X = pd.DataFrame(X)
+        X = X.iloc[:,0]
+        
+        X_return = pd.DataFrame()
+
+        X_return['sin_grade'] = np.sin(2*np.pi*X/360)
+        X_return['cos_grade'] = np.cos(2*np.pi*X/360)
+
+        return np.array(X_return)
+
+
+class CosSinMesDia(TransformerMixin,BaseEstimator):
+
+    def __init__(self, input_col):
+        
+        self.input_col = input_col
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+
+        months_in_year = 12
+        day_in_month = 30
+
+        X_return = pd.DataFrame()
+
+        X_return['sin_mes'] = np.sin(2*np.pi*X[self.input_col].dt.month/months_in_year)
+        X_return['cos_mes'] = np.cos(2*np.pi*X[self.input_col].dt.month/months_in_year)
+        
+        X_return['sin_day'] = np.sin(2*np.pi*X[self.input_col].dt.day/day_in_month)
+        X_return['cos_day'] = np.cos(2*np.pi*X[self.input_col].dt.day/day_in_month)
+
+        
+        return np.array(X_return)
+    
+ 
